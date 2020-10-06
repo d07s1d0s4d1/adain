fc9f1513a2c43ce0201eb3e993ce796c0d7ac18a
Dmitry <dsd_kem@mail.ru>
2020-10-06 19:56:07 +0300

    plot examples

---
+++ plot_validation.py
@@ -0,0 +1,18 @@
+from loss_model import create_model
+from generator import gen
+import itertools
+from utils import plot
+from tensorflow.keras.models import Model
+
+
+if __name__ == "__main__":
+    loss_model = create_model(with_loss=False)
+    loss_model.load_weights('weights/weights.h5')
+    model = Model(inputs=loss_model.inputs, outputs=loss_model.get_layer('decoded').output)
+    model.load_weights('weights/weights.h5')
+
+    g = gen('images/validation', batch_size=30)
+    (C, S), _ = next(g)
+    pred = model.predict([C, S])
+
+    plot(itertools.chain(*zip(C, S, pred)), 6, 6, (24,24))
